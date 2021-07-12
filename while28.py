1e5d0419a7eff457a9930fcf59bea7450cae03ac
Mansurjon Muhammadovich Kamolov <64670025+Mansurjon1112@users.noreply.github.com>
2021-07-12 12:05:46 +0500

    Update while28.py

--- while28.py
+++ while28.py
@@ -13,13 +13,16 @@ Created on Wed Jun 30 06:40:29 2021
 
 e=float(input('e='))
 if e>0 :
-    a=2
+    a1=2
+    a=0
     k=1
-    while abs(2+1/a-a)>=e:
-        k+=1
-        ak=2+1/a
-        ak,a=a,ak
-    print(k+1)
+    while abs(a-a1)>=e:
+        k+=1 
+        a=2+1/a1
+        a1,a=a,a1
+    print(k)    
+    print(a)
+    print(a1)
     
 else:
     print('e soni musbat bo\'lishi kerak!')
