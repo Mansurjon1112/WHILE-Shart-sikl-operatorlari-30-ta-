829f7cd5dc1131e93a4e0eafc1eb7b3778142984
Mansurjon Muhammadovich Kamolov <64670025+Mansurjon1112@users.noreply.github.com>
2021-07-12 10:05:52 +0500

    Update while20.py

--- while20.py
+++ while20.py
@@ -8,6 +8,7 @@ Created on Tue Jun 29 19:35:46 2021
 @author: Mansurjon Kamolov
 """
 
+#1-usul
 n=int(input('n='))
 if n>0:
     borligi = False
@@ -23,4 +24,14 @@ if n>0:
         print('2 raqami mavjud emas')
 else:
     print('Masala shartiga mos son kiritilmadi! n>0 bo\'lishi kerak edi')
-    
+
+#2-usul 
+n = int(input("n= "))
+while n > 0:
+    a = n % 10
+    if a == 2:
+        print('2 bor')
+        break
+    n = n // 10
+else:
+    print('2 mavjud emas')
